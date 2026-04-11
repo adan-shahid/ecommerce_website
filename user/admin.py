acf5362710dfbe13eae1c7a55f2bf9c77538075e
adan-shahid <adanshahid.student@gmail.com>
2026-04-11 22:12:32 +0500

    updated user/models.py

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
-# Register your models here.
+# from user import models
+# # Register your models here.
+# admin.site.register(models.User)  
--- user/models.py
+++ user/models.py
@@ -1,11 +1,61 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager
+)
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password = None):
+        if not email:
+            raise ValueError("Users must have an email address ")
+        user = self.model(
+            email = self.normalize_email(email)
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
+class User(AbstractBaseUser):
+    email = models.EmailField(max_length=255,unique=True)
+    # full_name = models.CharField(max_length=255, blank=True, null=True)
+    active = models.BooleanField(default=True)
+    staff = models.BooleanField(default=False)  # staff user, non superuser
+    admin = models.BooleanField(default=False) # superuser
+
+    USERNAME_FIELD = 'email'
+
+    REQUIRED_FIELDS = []
+    
+    object = UserManager()
 
     def __str__(self):
-        return self.name   
+        return self.email
+    
+    
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+    
+    @property
+    def is_staff(self):
+        return self.staff
+
+    @property
+    def is_admin(self):
+        return self.admin
+    
+    @property
+    def is_active(self):
+        return self.active
+    
+
+# class User(models.Model):
+#     name = models.CharField(max_length=100)
+#     email = models.EmailField(unique=True)
+#     password = models.CharField(max_length=100)
+
+#     def __str__(self):
+#         return self.name   
