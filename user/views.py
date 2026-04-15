6311fb1daeff4c97a8bac5bd2b9e45a48fcb5b49
adan-shahid <adanshahid.student@gmail.com>
2026-04-15 22:46:24 +0500

    added registration view and html page

--- user/views.py
+++ user/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserRegistrationForm
+from django.contrib.auth import login
 
 
 # Create your views here.
@@ -8,3 +9,13 @@ from .forms import UserRegistrationForm
 def gretting(request):
     return render(request, 'index.html')
 
+def register_user(request):
+    form = UserRegistrationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request,user)
+        return redirect('greeting')
+    context = {
+        'form':form,
+    }
+    return render(request, 'register.html', context)
