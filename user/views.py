db3ec8b6217b0cc574e88221a6ce313c73b38ed0
adan-shahid <adanshahid.student@gmail.com>
2026-04-15 22:38:32 +0500

    Created the UserRegistrationForm

--- user/views.py
+++ user/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import UserRegistrationForm
+
 
 # Create your views here.
 
 def gretting(request):
     return render(request, 'index.html')
+
