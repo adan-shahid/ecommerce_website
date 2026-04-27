0dfb5d1d38bdcf73e76d541e33c48ce40ee7146f
adan-shahid <adanshahid.student@gmail.com>
2026-04-27 08:32:43 +0500

    write the forms.py for users app

--- user/forms.py
+++ user/forms.py
@@ -1,1 +1,13 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import customUser
+
+class customUserCreationForm(UserCreationForm):
+    class Meta:
+        model = customUser
+        fields = ('username', 'email', 'phone_number')
+
+class customUserChangeForm(UserChangeForm):
+    class Meta:
+        model = customUser
+        fields = ('username', 'email', 'phone_number')
