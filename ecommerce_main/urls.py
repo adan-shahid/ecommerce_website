752b700348e1a760864c3e9e1717c2fa78de5edc
adan-shahid <adanshahid.student@gmail.com>
2026-04-30 22:06:16 +0500

    completed the basic setup of showing product

--- ecommerce_main/urls.py
+++ ecommerce_main/urls.py
@@ -23,6 +23,8 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user.urls')),
+    path('product/', include('products.urls')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def product_View(request):
+    products = Product.objects.all()
+    context = {
+        'products':products
+    }
+    return render(request, 'product/product.html', context)
