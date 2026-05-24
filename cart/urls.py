b514ab42f3e06f82a59b0b7f32d4490245aca98a
adan-shahid <adanshahid.student@gmail.com>
2026-05-24 20:42:58 +0500

    updated cart/views, urls

--- cart/urls.py
+++ cart/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
-    
 
+    path('add-to-cart/</product_id>', views.add_to_cart, name = 'add-to-cart'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from products.models import Product
 
 # Create your views here.
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id = product_id)
+    cart = request.session.get('cart', {})
+    product_id_str = str(product_id)
+
+    if product_id_str in cart:
+        cart[product_id_str] += 1
+    else:
+        cart[product_id_str] = 1   
+
+    request.session['cart'] = cart
+    request.session.modifed = True
+    return redirect('index')
