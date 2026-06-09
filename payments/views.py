a1e9fcaa6955ffb6f8447387ddec9ce4cfa7472e
adan-shahid <adanshahid.student@gmail.com>
2026-06-09 22:05:19 +0500

    updated the views

--- payments/views.py
+++ payments/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from cart.models import CartItem, Cart
 
 # Create your views here.
 def payment(request):
+
+    cart_items = CartItem.objects.filter(cart__user = request.user)
+    print(cart_items)
+    if not cart_items.exists():
+        return redirect('product')
+
     return HttpResponse('This is for payments page')
