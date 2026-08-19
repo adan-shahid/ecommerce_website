6eee4bdc1c0b4d0535bceb40532b0e87baf1deed
adan-shahid <adanshahid.student@gmail.com>
2026-08-19 21:31:55 +0500

    added the payment processing urls

--- payments/urls.py
+++ payments/urls.py
@@ -3,7 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.create_checkout_session, name='create_checkout_session'),
+    path('create-checkout-session/', views.create_checkout_session, name='create_checkout_session'),
+    path('success/', views.payment_success, name='payment_success'),
+    path('cancel/', views.payment_cancel, name='payment_cancel'),
     
 
 ]
--- payments/views.py
+++ payments/views.py
@@ -6,15 +6,6 @@ import stripe
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
-def payment(request):
-
-    cart_items = CartItem.objects.filter(cart__user = request.user)
-    print(cart_items)
-    if not cart_items.exists():
-        return redirect('product')
-
-    return HttpResponse('This is for payments page')
-
 def create_checkout_session(request):
     try:
         user_cart = request.user.cart
